c4b3bda8af38fda6eebb5114a8ab6917adef9310
ngogosashvili <114146743+ngogosashvili@users.noreply.github.com>
2023-10-14 02:23:47 +0400

    Create examples.js

---
+++ examples.js
@@ -0,0 +1,96 @@
+//ცვლადები
+
+//var 
+//let
+//const
+
+
+
+//ტიპები
+
+//string
+// let str = "Name";
+// console.log(typeof(str));
+
+// //number
+// let num = 100;
+// console.log(typeof(num));
+
+//bool
+// let bl = 1 != 1;
+// console.log(bl);
+// console.log(typeof(bl));
+
+//symbol
+// let smb = Symbol("symbol");
+// let smb2 = Symbol("symbol");
+
+
+// console.log(smb);
+// console.log(smb2);
+
+// console.log(typeof(smb));
+// console.log(typeof(smb2));
+
+// console.log(smb == smb2);
+
+
+//undefined
+// let und = undefined;
+// console.log(und);
+// console.log(typeof(und));
+
+//object
+// let obj = {
+//     name: "Ann",
+//     lastName: "LastName"
+// }
+// console.log(obj.name);
+// console.log(typeof(obj));
+
+
+// let title = "title";
+// title = null;
+// console.log(typeof(title));
+
+// let nmb = 0;
+// console.log(nmb);
+// console.log(typeof(nmb));
+// console.log(-0 == 0);
+// console.log(nmb.toString());
+
+// let arr = [1, 2, null, NaN, 3, "a", undefined];
+// console.log(arr);
+
+// console.log(NaN === NaN);
+// console.log(Object.is(NaN, NaN));
+
+
+// ტიპების კონვერტაცია
+// //toString
+
+// let num = 1;
+// let str = num.toString();
+// console.log(typeof(str));
+
+// let num = 1;
+// let str = "1";
+// let result = 1 + str;
+
+// let arr = [1, 2, 5];
+
+// let obj = {
+//     name: "Ann",
+//     lastName: "LastName"
+// }
+// console.log(obj.toString());
+
+
+
+//toNumber
+// console.log("string" - 1);
+// console.log("11" - 1);
+// console.log(true + 1);
+// console.log(false + 1);
+// console.log(null + 1);
+// console.log(undefined + 1);
